e226651555488e7103eb18fe56b2619847e5328b
JoshRobertson9 <rosh.jobertson@gmail.com>
2024-07-20 21:54:20 -0400

    starting the project

---
+++ main.py
@@ -0,0 +1,20 @@
+
+from matplotlib import pyplot as plt
+import csv
+
+
+def main():
+	pass
+
+
+def read_csv_columns(filename):
+# Reads in a csv that is a continuous unknown number of columns with the data label in the first index.
+
+	with open(filename) as file:
+		pass
+
+	return  csv_array
+
+
+if  __name__ == "__main__":
+	main()
